12058961c8f2e75933e12467ec8c12f2173f2377
district2 <111503426+district2@users.noreply.github.com>
2023-12-30 15:33:50 +0100

    style: convert to tuples

--- components/build-card.tsx
+++ components/build-card.tsx
@@ -8,18 +8,18 @@ import Button from "./button";
 export default function BuildCard({ created_at, android_version, variant, version, download_link }:
     { created_at: string, android_version?: string, variant: "Vanilla" | "Gapps", version: string, download_link: string }) {
 
-    const info =
+    const info: Array<[any, string, string]> =
         new Array(
-            { image: calendar, name: "Date", value: created_at },
-            { image: versionImage, name: "Version", value: version },
-            { image: androidImage, name: "Android", value: android_version! },
-            { image: variantImage, name: "Variant", value: variant },
+            [calendar, "Date", created_at],
+            [versionImage, "Version", version],
+            [androidImage, "Android", android_version!],
+            [variantImage, "Variant", variant],
         )
 
     return (
         <div className="flex flex-col gap-y-2 font-medium bg-neutral-950 p-6 rounded-lg">
             {
-                info.map(({ image, name, value }, i) =>
+                info.map(([image, name, value], i) =>
                     <div className="flex gap-x-3" key={i}>
                         <Image src={image} alt={`${name} icon`} />
                         <p className="text-white/70">{name}: <span className="text-white">{value}</span></p>
