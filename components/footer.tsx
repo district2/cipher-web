90d50c2cb91f53330187782098a9828dfb8186b5
district2 <111503426+district2@users.noreply.github.com>
2024-03-04 16:24:43 +0100

    fix: revamp footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,48 +1,47 @@
+import telegram from "@/public/icons/telegram.svg"
+import twitter from "@/public/icons/twitter.svg"
+import github from "@/public/icons/github.svg"
 import Link from "next/link";
-import FooterLink from "./footer-link";
+import Image from "next/image";
+
+const SOCIALS = [
+	{
+		icon: telegram,
+		url: "https://t.me/CipherOS",
+		alt: "Telegram icon"
+	},
+	{
+		icon: twitter,
+		url: "https://twitter.com/CipherROM",
+		alt: "Twitter icon"
+	},
+	{
+		icon: github,
+		url: "https://github.com/CipherOS",
+		alt: "Github icon"
+	},
+]
 
 export default function Footer() {
 	return (
-		<footer className="relative mt-48 w-full bg-neutral-950/60 md:h-64">
-			<div className="flex flex-col gap-y-3 items-center p-4 pb-20 h-full md:justify-between md:flex-row md:px-16 md:pb-0">
-				<h1 className="text-2xl font-medium">CipherOS</h1>
-				<div className="flex flex-col gap-7 md:flex-row">
-					<div className="flex-col gap-y-3">
-						<h1 className="text-lg">Navigation</h1>
-						<div className="flex flex-col gap-y-2">
-							<FooterLink name="Home" link="/" />
-							<FooterLink name="Devices" link="/devices" />
-							<FooterLink name="Blog" link="/blog" />
-						</div>
-					</div>
-					<div className="flex-col gap-y-3">
-						<h1 className="text-lg">Contacts</h1>
-						<div className="flex flex-col gap-y-2">
-							<FooterLink
-								name="GitHub"
-								link="https://github.com/cipheros"
-								ext
-							/>
-							<FooterLink name="Telegram" link="https://t.me/cipheros" ext />
-							<FooterLink
-								name="Twitter"
-								link="https://twitter.com/cipherrom"
-								ext
-							/>
-						</div>
-					</div>
+		<footer className="relative mt-48 w-full bg-neutral-950/60">
+			<div className="flex text-sm py-8 p-4 items-center justify-between flex-col gap-y-3 md:flex-row">
+				<p className="text-neutral-500 text-center order-3 pt-6 md:order-none md:pt-0">
+					Copyright © 2024 by CipherOS.
+					<br />
+					All Rights Reserved.
+				</p>
+				<p className="text-neutral-400">
+					CipherOS <span className="text-white">❤</span> Open Source
+				</p>
+				<div className="flex gap-x-3">
+					{SOCIALS.map((social) =>
+						<Link href={social.url} key={social.alt} target="_blank">
+							<Image src={social.icon} alt={social.alt} width={30} className="transition-opacity opacity-75 hover:opacity-100" />
+						</Link>
+					)}
 				</div>
 			</div>
-			<p className="absolute bottom-4 w-full text-sm font-medium text-center">
-				Made by{" "}
-				<Link
-					href="https://github.com/district2"
-					target="_blank"
-					className="font-mono underline underline-offset-4"
-				>
-					district2
-				</Link>
-			</p>
 		</footer>
 	);
 }
