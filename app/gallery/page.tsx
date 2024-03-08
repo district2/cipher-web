e546128fde2bf1f9c943287f542285b3281c19e3
district2 <111503426+district2@users.noreply.github.com>
2024-03-08 12:46:25 +0100

    fix: remove min height css property because was causing weird image rendering on chromium based browsers

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -13,7 +13,7 @@ export default async function Gallery() {
             <div className="flex flex-wrap gap-12 justify-center">
                 {
                     files.map((image, i) =>
-                        <Image className="w-auto min-h-[410px] max-h-[30vw]" src={`/images/gallery/${image}`} key={image}
+                        <Image className="w-auto h-auto" src={`/images/gallery/${image}`} key={image}
                             alt={`CipherOS screen ${i}`} width={300} height={500} />
                     )
                 }
